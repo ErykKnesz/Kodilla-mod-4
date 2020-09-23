7995ae6db106f5244aee626f6cd52099612ee1ac
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-23 20:50:24 +0200

    Finish the palindrom_check function with added input feature

--- zad1_mod4_palindromy.py
+++ zad1_mod4_palindromy.py
@@ -1,14 +1,17 @@
-def palindrom_check(word):
-    '''Returns True if the string spelled backwards is the same as spelled forwards,
-    otherwise returns False'''
+def palindrom_check(word=str(input())):
+    '''Returns True if the input string spelled backwards is the same as spelled forwards,
+    otherwise returns False:
+    e.g attribute lol == True
+    roftl == False'''
     letters = []
     letters_2 = []
     for i in word:
-        letters = letters.append(i)
-    for i_2 in word[-1]:
-        letters_2 = letters_2.append(i_2)
+        letters.append(i)
+    for i_2 in word[::-1]:
+        letters_2.append(i_2)
     if letters == letters_2:
         return True
     else:
         return False
-palindrom_check(word)
+print(palindrom_check(word=str(input())))
+
