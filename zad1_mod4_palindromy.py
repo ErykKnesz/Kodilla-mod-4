4161be38a105b9b331dd7ffd63f9fad86d0a1ab2
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-23 20:34:04 +0200

    create prototype function palindrom_check

--- zad1_mod4_palindromy.py
+++ zad1_mod4_palindromy.py
@@ -1,2 +1,14 @@
-def palindrom_check():
-    
+def palindrom_check(word):
+    '''Returns True if the string spelled backwards is the same as spelled forwards,
+    otherwise returns False'''
+    letters = []
+    letters_2 = []
+    for i in word:
+        letters = letters.append(i)
+    for i_2 in word[-1]:
+        letters_2 = letters_2.append(i_2)
+    if letters == letters_2:
+        return True
+    else:
+        return False
+palindrom_check(word)
