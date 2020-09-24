8b77cfe383ad09f48aecaa5c712f9d68fed324a0
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-24 19:07:50 +0200

    Add isalpha() method and optimised readability

--- zad1_mod4_palindromy.py
+++ zad1_mod4_palindromy.py
@@ -1,18 +1,25 @@
-def palindrom_check(word):
+def palindrom_check():
     '''Returns True if the input string spelled backwards is the same as spelled forwards,
     otherwise returns False:
     e.g attribute lol == True
     roftl == False'''
+    word=input("Please provide your word or phrase: ").lower()
     letters = []
     letters_2 = []
     for i in word:
-        letters.append(i)
+        if i.isalpha() == False:
+            pass
+        else:
+            letters.append(i)
     for i_2 in word[::-1]:
-        letters_2.append(i_2)
+        if i_2.isalpha() == False:
+            pass
+        else:
+            letters_2.append(i_2)
     if letters == letters_2:
         return True
     else:
         return False
-print(palindrom_check(word=input().lower()))
-
-
+    
+    
+print(palindrom_check())
