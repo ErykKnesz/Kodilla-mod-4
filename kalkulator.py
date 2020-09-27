6a3edad9db762f46c43b3f0c428bb354f2bd4edd
Eryk Knesz <eryk.knesz@gmail.com>
2020-09-27 10:16:01 +0200

    Change Polish diactric signs in variables names

--- kalkulator.py
+++ kalkulator.py
@@ -3,46 +3,46 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 def kalkulator():
-    działanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+    dzialanie = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
     
     dodawanie = 0
-    mnożenie = 1
+    mnozenie = 1
 
-    if działanie <= 4:
+    if dzialanie <= 4:
         num = float(input("Podaj pierwszą liczbę: "))
         num_2 = float(input("Podaj drugą liczbę: "))
     else:
-        działanie = int(input("Koniecznie podaj liczbę z zakresu 1-4 dla: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+        dzialanie = int(input("Koniecznie podaj liczbę z zakresu 1-4 dla: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
         num = float(input("Podaj pierwszą liczbę: "))
         num_2 = float(input("Podaj drugą liczbę: "))
 
-    while działanie == 1:
+    while dzialanie == 1:
         prompt_1 = input("Podaj kolejną liczbę, albo wciśnij x, by zakończyć: ")
         if prompt_1 == 'x':
             break
         if prompt_1 != 'x':
             logging.info("Najpierw bliczam sumę dodatkowych liczb")
             dodawanie = dodawanie + float(prompt_1)
-    if działanie == 1:         
+    if dzialanie == 1:         
         logging.info("Dodaję %s i %s do sumy %s" %(num, num_2, dodawanie))        
         print(num + num_2 + dodawanie)
     
-    if działanie == 2:
+    if dzialanie == 2:
         logging.info("Odejmuję %s i %s" %(num, num_2))
         print(num - num_2)
 
-    while działanie == 3:
+    while dzialanie == 3:
         prompt_1 = input("Podaj kolejną liczbę, albo wciśnij x, by zakończyć: ")
         if prompt_1 == 'x':
             break
         if prompt_1 != 'x':
             logging.info("Najpierw mnożę dodatkowe liczby")
-            mnożenie = mnożenie * float(prompt_1)
-    if działanie == 3:        
-        logging.info("Mnożę %s przez %s i otrzymany iloczyn %s" %(num, num_2, mnożenie))        
-        print(num * num_2 * mnożenie)
+            mnozenie = mnozenie * float(prompt_1)
+    if dzialanie == 3:        
+        logging.info("Mnożę %s przez %s i otrzymany iloczyn %s" %(num, num_2, mnozenie))        
+        print(num * num_2 * mnozenie)
 
-    if działanie == 4:
+    if dzialanie == 4:
         if num_2 != 0:
             logging.info("Dzielę %s przez %s" %(num, num_2))
             print(num / num_2)
