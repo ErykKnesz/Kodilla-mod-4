a300d792926172a86f07285c98a269d5ef172831
Eryk Knesz <eryk.knesz@gmail.com>
2020-10-01 21:17:10 +0200

    fix formatting issues

--- kalkulator_v2.py
+++ kalkulator_v2.py
@@ -1,6 +1,7 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
+
 def input_control(num, message):
     while num is None:
         text = input(message)
@@ -10,30 +11,33 @@ def input_control(num, message):
             pass
     return num
 
-def input_control2(num,message):
+
+def input_control2(num, message):
     while num is False:
         text = input(message)
         try:
             return float(text)
         except ValueError:
             if text.lower() == 'x':
-              return text
+                return text
     return num
 
-def input_control3(num,message):
+
+def input_control3(num, message):
     while num is True:
         text = input(message)
         try:
             if text in '1234':
                 return str(text)
-        except ValueError: 
+        except ValueError:
             pass
-    return num  
+    return num
+
 
 def get_data(operation):
 
-    a = input_control(None,"Podaj pierwszą liczbę: ")
-    b = input_control(None,"Podaj drugą liczbę: ")
+    a = input_control(None, "Podaj pierwszą liczbę: ")
+    b = input_control(None, "Podaj drugą liczbę: ")
     others = []
     if operation in "12":
         while True:
@@ -47,37 +51,48 @@ def get_data(operation):
 
 
 def add(a, b, *others):
-    logging.info("Dodaję %s i %s do sumy %s" %(a,b,others))
+    logging.info("Dodaję %s i %s do sumy %s" % (a, b, others))
     return a + b + sum(others)
+
 
 def mul(a, b, *others):
     res = a * b
     for i in others:
         res = res * i
-    logging.info("Mnożę %s przez %s i otrzymany iloczyn %s" %(a,b,others))
+    logging.info("Mnożę %s przez %s i otrzymany iloczyn %s" % (a, b, others))
     return res
 
-def sub(a,b):
-    logging.info("Odejmuję %s i %s" %(a,b))
-    return a-b
 
-def div(a,b):
+def sub(a, b):
+    logging.info("Odejmuję %s i %s" % (a, b))
+    return a - b
+
+
+def div(a, b):
     if b != 0:
-        logging.info("Dzielę %s przez %s" %(a, b))
-        return a/b
+        logging.info("Dzielę %s przez %s" % (a, b))
+        return a / b
     else:
         logging.info("Nie dziel przez zero")
         exit(1)
 
+
 operations = {
-	"1": add,
-	"2": mul,
+    "1": add,
+    "2": mul,
     "3": sub,
     "4": div
 }
 
 if __name__ == "__main__":
-    operation = input_control3(True,"Podaj działanie, posługując się odpowiednią liczbą: \n 1 Dodawanie,\n 2 Mnożenie,\n 3 Odejmowanie,\n 4 Dzielenie: \n ")
+    operation = input_control3(
+        True,
+        "Podaj działanie, posługując się odpowiednią liczbą:\n\
+         1 Dodawanie,\
+         \n 2 Mnożenie,\
+         \n 3 Odejmowanie,\
+         \n 4 Dzielenie:\
+         \n ")
     a, b, others = get_data(operation)
     result = operations[operation](a, b, *others)
     print(result)
