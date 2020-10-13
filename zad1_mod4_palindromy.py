089726a92e2f6f7f6ed77c1a4f986312a5c44e90
Eryk Knesz <eryk.knesz@gmail.com>
2020-10-13 20:48:27 +0200

    add parameter and argument instead of input within function

--- zad1_mod4_palindromy.py
+++ zad1_mod4_palindromy.py
@@ -1,25 +1,28 @@
-def palindrom_check():
-    '''Returns True if the input string spelled backwards is the same as spelled forwards,
+word = "Do geese see God?"
+
+
+def palindrom_check(word):
+    '''Returns True if the string in variable word
+    spelled backwards is the same as spelled forwards,
     otherwise returns False:
     e.g attribute lol == True
     roftl == False'''
-    word=input("Please provide your word or phrase: ").lower()
     letters = []
     letters_2 = []
-    for i in word:
-        if i.isalpha() == False:
+    for letter in word.lower():
+        if letter.isalpha() is False:
             pass
         else:
-            letters.append(i)
-    for i_2 in word[::-1]:
-        if i_2.isalpha() == False:
+            letters.append(letter)
+    for letter_2 in word[::-1].lower():
+        if letter_2.isalpha() is False:
             pass
         else:
-            letters_2.append(i_2)
+            letters_2.append(letter_2)
     if letters == letters_2:
         return True
     else:
         return False
-    
-    
-print(palindrom_check())
+
+
+print(palindrom_check(word))
